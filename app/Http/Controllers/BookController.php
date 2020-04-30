2f96888dd68c52705a84132b920fcafefe451758
alfin222 <alfin_2754@apps.ipb.ac.id>
2020-04-30 16:59:31 +0700

    added JWT auth

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class BookController extends Controller
+{
+    public function book() {
+        $data = "Data All Book";
+        return response()->json($data, 200);
+    }
+
+    public function bookAuth() {
+        $data = "Welcome " . Auth::user()->name;
+        return response()->json($data, 200);
+    }
+}
